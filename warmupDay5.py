a6ea53c2dfc53bd66bbae69691367787059e913a
rahiii <rahimitranyc@gmail.com>
2019-03-09 15:32:53 +0000

    Warm Up

--- warmupDay5.py
+++ warmupDay5.py
@@ -1,1 +1,6 @@
 # File name: warmupDay5.py
+guess = input("Guess my name! ")
+while guess != "Rahi":
+    print("That's not my name. It rhymes with Mahi")
+    guess = input("Try Again ")
+print ("you guessed right!")
